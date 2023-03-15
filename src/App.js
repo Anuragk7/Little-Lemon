7db73afc31db6c35fb511f0459f875f3914e249b
Anurag Khugshal <113304552+Anuragk7@users.noreply.github.com>
2023-03-15 11:56:36 +0530

    adding resources to the public folder

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <header className="App-header">
-       <meta name = "description" value = "Little Lemon is the ultimate destination for you to end your hunger. Don't feel like getting up, we'll get your food to you!"/>
-       <meta name = "title" value = "Little Lemon"/>
+       <meta name = "og:title" value = "Little Lemon"/>
+       <meta name = "og:description" value = "Little Lemon is the ultimate destination for you to end your hunger. Don't feel like getting up, we'll get your food to you!"/>
        <meta name = "language" value = "english"/>
        <meta name="HandheldFriendly" content="true"/>
        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
