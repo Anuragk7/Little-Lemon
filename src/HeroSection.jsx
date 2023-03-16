f6d3cbacdfa498baa7b375e4d48a5eda33217ab9
Anurag Khugshal <113304552+Anuragk7@users.noreply.github.com>
2023-03-16 12:48:54 +0530

    designed the hero section

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,9 +1,16 @@
+
 function HeroSection() {
- return <>
- <div className="hero-text">
-    <h1>Little Lemon</h1>
-    <h5>Chicago</h5>
- </div>
- </>
+ return (<div className="herosection">
+   <div className="hero-text">
+      <h1>Little Lemon</h1>
+      <h3>Chicago</h3>
+      <p>Combining traditional techniques with modern taste, at Little 
+         Lemon dishes which don't just make you full but make you happy!</p>
+         <button>Reserve a table</button>
+   </div>
+   <div className="hero-pic">
+      
+   </div>
+ </div>);
 }
 export default HeroSection;
